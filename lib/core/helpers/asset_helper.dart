ec83c3224858642437a524f0d2030128e881f68f
ddongkma <ddongkma@gmail.com>
2023-07-21 13:50:21 +0700

    init

---
+++ lib/core/helpers/asset_helper.dart
@@ -0,0 +1,4 @@
+class AssetHelper{
+  static const String imageBackGroundSplash = 'assets/images/background_splash.png';
+  static const String circleSplash = 'assets/images/circle_splash.png';
+}
---
+++ lib/representation/screens/splash_screen.dart
@@ -0,0 +1,24 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:travel_app/core/helpers/asset_helper.dart';
+import 'package:travel_app/core/helpers/image_helper.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+static String routeName = '/splash_screen';
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned.fill(child: ImageHelper.loadFromAsset(AssetHelper.imageBackGroundSplash,fit:BoxFit.fitWidth)),
+        Positioned.fill(child: ImageHelper.loadFromAsset(AssetHelper.circleSplash,))
+      ],
+    );
+  }
+}
